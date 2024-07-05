a8cb978548924877f8f7d5fdfbd485634578f254
bcullerton <bcullerton@companieshouse.gov.uk>
2024-07-05 10:16:51 +0100

    Use spring constants for header names

--- src/main/java/uk/gov/companieshouse/api/filter/CustomCorsFilter.java
+++ src/main/java/uk/gov/companieshouse/api/filter/CustomCorsFilter.java
@@ -3,6 +3,7 @@ package uk.gov.companieshouse.api.filter;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsUtils;
 
 import jakarta.servlet.Filter;
@@ -17,10 +18,6 @@ public class CustomCorsFilter implements Filter {
 
     private List<String> externalMethods;
     private String OPTIONS_METHOD = "OPTIONS";
-    private String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
-    private String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
-    private String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
-    private String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
     private String ERIC_ALLOWED_ORIGIN = "ERIC-Allowed-Origin";
     
     public CustomCorsFilter(List<String> externalMethods) {
@@ -36,13 +33,14 @@ public class CustomCorsFilter implements Filter {
 
         if (CorsUtils.isCorsRequest(httpServletRequest)) {
             if (method.equalsIgnoreCase(OPTIONS_METHOD)) {
-                httpServletResponse.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-                httpServletResponse.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, "*");
-                httpServletResponse.setHeader(ACCESS_CONTROL_MAX_AGE, "3600");
+                httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+                httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+                httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*");
+                httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
                 httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
                 return;
             } else {
-                httpServletResponse.setHeader(ACCESS_CONTROL_ALLOW_METHODS, String.join(",",externalMethods));
+                httpServletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, String.join(",",externalMethods));
                 String allowedOrigin = httpServletRequest.getHeader(ERIC_ALLOWED_ORIGIN);
                 if (!externalMethods.contains(method) || (allowedOrigin == null || allowedOrigin.isEmpty())) {
                     httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "cors forbidden error");
